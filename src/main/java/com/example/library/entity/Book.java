fdf67df1cb26b5e2f3044e8c2fe05a8c3a57b60c
DMRSCK DIvisekara <10charithyaskd@gmail.com>
2025-06-04 21:52:19 +0530

    Updated code with comments and new functionality

--- src/main/java/com/example/library/entity/Book.java
+++ src/main/java/com/example/library/entity/Book.java
@@ -1,36 +1,37 @@
 package com.example.library.entity;
 
-import jakarta.persistence.Entity;//Imports JPA annotations to map this class to a database table.
+import jakarta.persistence.Entity;// Brings in tools to connect this class to a database table
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
-import jakarta.validation.constraints.NotBlank;// Import to use validation annotation for non-empty fields.
+import jakarta.validation.constraints.NotBlank;// Adds checks to make sure some fields are not empty
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
 
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class Book {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@Entity // Says this class will be saved in the database as a table.
 
-    @NotBlank(message = "Title is required")
+@Data // Automatically creates getters, setters, and other common methods.
+@NoArgsConstructor // Creates a constructor that needs no inputs.
+@AllArgsConstructor // Creates a constructor that uses all fields as inputs.
+@Builder // Lets you build objects in a flexible way.
+public class Book {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //PK
+
+    @NotBlank(message = "Title is required") // Makes sure the title is not empty.
     private String title;
 
-    @NotBlank(message = "Author is required")
+    @NotBlank(message = "Author is required")// Makes sure the author name is not empty.
     private String author;
 
-    @NotBlank(message = "ISBN is required")
+    @NotBlank(message = "ISBN is required") // Makes sure the ISBN is not empty
     @Column(unique = true)
     private String isbn;
     @Builder.Default
-    private boolean available = true;
+    private boolean available = true; // Shows if the book is available to borrow, true by default.
 }
